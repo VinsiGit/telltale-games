5a4d8c154ce80c00e33a7cf7cab3cca8dc0f9175
jVaerewijck <vaerewijckjarn@gmail.com>
2024-12-16 13:38:32 +0100

    basis

--- Assets/Scripts/time_travel.cs
+++ Assets/Scripts/time_travel.cs
@@ -6,15 +6,13 @@ using UnityEngine.SceneManagement;
 
 public class time_travel : MonoBehaviour
 {
-    public float scanTime = 3.0f;
-    private float timer = 0f;
-    private bool isScanning = false;
-    private bool isBeingGrabbed = false;
+    public GameObject rotatingObject;
     private UnityEngine.XR.Interaction.Toolkit.Interactables.XRGrabInteractable grabInteractable;
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("started");
+        rotatingObject.SetActive(false);
         grabInteractable = GetComponent<UnityEngine.XR.Interaction.Toolkit.Interactables.XRGrabInteractable>();
 
         grabInteractable.selectEntered.AddListener(OnGrab);
@@ -22,71 +20,41 @@ public class time_travel : MonoBehaviour
     }
     void OnDestroy()
     {
-        // Unregister grab events to avoid memory leaks
         grabInteractable.selectEntered.RemoveListener(OnGrab);
         grabInteractable.selectExited.RemoveListener(OnRelease);
     }
 
     void OnGrab(SelectEnterEventArgs args)
     {
-        isBeingGrabbed = true;
         Debug.Log("grab");
-        timer = 0f; // Reset the timer when grabbed
+        TriggerAction();
     }
 
     void OnRelease(SelectExitEventArgs args)
     {
-        isBeingGrabbed = false;
-        timer = 0f; // Reset the timer when released
+
     }
     void OnTriggerEnter(Collider other)
     {
         Debug.Log(other);
         if (other.CompareTag("Controller"))
         {
-            isScanning = true;
-            timer = 0f;
-            Debug.Log(isScanning);
-        }
-    }
-    void OnTriggerStay(Collider other)
-    {
-        Debug.Log(other);
-        if (isScanning && other.CompareTag("Controller"))
-        {
-            timer += Time.deltaTime;
-            if (timer >= scanTime)
-            {
-                TriggerAction();
-                isScanning = false;
-            }
+            TriggerAction();
         }
     }
     void OnTriggerExit(Collider other)
     {
         if (other.CompareTag("Controller"))
         {
-            isScanning = false;
-            timer = 0f;
+
         }
     }
     void TriggerAction()
     {
         Debug.Log("Scan Complete!");
-        SceneManager.LoadScene("RomanScene");
+        rotatingObject.SetActive(true);
     }
-    // Update is called once per frame
     void Update()
     {
-        if (isBeingGrabbed)
-        {
-            timer += Time.deltaTime;
-            if (timer >= scanTime)
-            {
-                TriggerAction();
-                isBeingGrabbed = false; // Prevent re-triggering
-                timer = 0f; // Reset the timer
-            }
-        }
     }
 }
